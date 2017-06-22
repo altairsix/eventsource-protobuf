dcb738986f029a2a7b1c67b59f7ef4577400338b
savaki <matt.ho@gmail.com>
2017-06-22 09:17:39 -0700

    - added ability to serialize / deserialize streams

--- cmd/eventsource-protobuf/generate/file.go
+++ cmd/eventsource-protobuf/generate/file.go
@@ -14,7 +14,11 @@ const (
 package {{ .Package }}
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"time"
 
 	"github.com/altairsix/eventsource"
@@ -88,6 +92,84 @@ func UnmarshalEvent(data []byte) (eventsource.Event, error) {
 	}
 
 	return event.(eventsource.Event), nil
+}
+
+type Encoder struct{
+	w io.Writer
+}
+
+func (e *Encoder) WriteEvent(event eventsource.Event) (int, error) {
+	data, err := MarshalEvent(event)
+	if err != nil {
+		return 0, err
+	}
+
+	// Write the length of the marshaled event as uint64
+	//
+	buffer := make([]byte, 8)
+	binary.LittleEndian.PutUint64(buffer, uint64(len(data)))
+	if _, err := e.w.Write(buffer); err != nil {
+		return 0, err
+	}
+
+	n, err := e.w.Write(data)
+	if err != nil {
+		return 0, err
+	}
+
+	return n + 8, nil
+}
+
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{
+		w: w,
+	}
+}
+
+type Decoder struct {
+	r       *bufio.Reader
+	scratch *bytes.Buffer
+}
+
+func (d *Decoder) readN(n uint64) ([]byte, error) {
+	d.scratch.Reset()
+	for i := uint64(0); i < n; i++ {
+		b, err := d.r.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		if err := d.scratch.WriteByte(b); err != nil {
+			return nil, err
+		}
+	}
+	return d.scratch.Bytes(), nil
+}
+
+func (d *Decoder) ReadEvent() (eventsource.Event, error) {
+	data, err := d.readN(8)
+	if err != nil {
+		return nil, err
+	}
+	length := binary.LittleEndian.Uint64(data)
+
+	data, err = d.readN(length)
+	if err != nil {
+		return nil, err
+	}
+
+	event, err := UnmarshalEvent(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return event, nil
+}
+
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder {
+		r:       bufio.NewReader(r),
+		scratch: bytes.NewBuffer(nil),
+	}
 }
 `
 )
