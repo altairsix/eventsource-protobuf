a6ce6ce3d9db2b76884cd762f57cb4bdd5c6c6b8
savaki <matt.ho@gmail.com>
2017-07-19 16:16:20 -0700

    - initial support for generating commands from protobuf

---
+++ examples/commands/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/altairsix/eventsource"
+)
+
+//go:generate protoc -I .:$GOPATH/src:$GOPATH/src/github.com/gogo/protobuf/protobuf --gogo_out=. commands.proto
+//go:generate protoc -I .:$GOPATH/src:$GOPATH/src/github.com/gogo/protobuf/protobuf --commands_out=. commands.proto
+
+func main() {
+	var cmd eventsource.Command
+
+	cmd = &RegisterUser{ID: "abc"}
+	fmt.Printf("%#v\n", cmd)
+
+	cmd = &ResetPassword{Id: "def"}
+	fmt.Printf("%#v\n", cmd)
+}
